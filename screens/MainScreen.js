e7b0328eb1fd3ee23269359015afd8f150a65945
Yurii Bolharyn <yurecpr@gmail.com>
2026-06-24 21:01:46 +0200

    v1.2.5: прод сервер, версія в MainScreen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -82,7 +82,7 @@ const [loading, setLoading] = useState(true);
         </View>
       </View>
       )}
-      <Text style={styles.versionText}>Версія 1.2.3</Text>
+      <Text style={styles.versionText}>Версія 1.2.5</Text>
     </View>
   );
 };
