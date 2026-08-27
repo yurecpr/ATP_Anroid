305be5511cdf0e0e212548d3b1eb192c6d49bac3
Yurii Bolharyn <yurecpr@gmail.com>
2026-08-27 14:00:21 +0200

    chore: prepare Android 1.3.01 beta build

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -82,7 +82,7 @@ const [loading, setLoading] = useState(true);
         </View>
       </View>
       )}
-      <Text style={styles.versionText}>Версія 1.3.0 beta</Text>
+      <Text style={styles.versionText}>Версія 1.3.01 beta</Text>
     </View>
   );
 };
