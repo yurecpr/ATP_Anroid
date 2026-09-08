eb5a69c83d81f6fed9e7eafd7c25774df091235d
Yurii Bolharyn <yurecpr@gmail.com>
2026-09-08 11:59:08 +0200

    Label no-report app variant

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -82,7 +82,7 @@ const [loading, setLoading] = useState(true);
         </View>
       </View>
       )}
-      <Text style={styles.versionText}>Версія 1.3.2 без звітів</Text>
+      <Text style={styles.versionText}>Версія 1.3.2 no report</Text>
     </View>
   );
 };
