0c6913c4714e70b6546a4f205861d3a45656a866
Yurii Bolharyn <yurecpr@gmail.com>
2026-09-08 12:01:42 +0200

    Show no-report label on home screen

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -82,7 +82,7 @@ const [loading, setLoading] = useState(true);
         </View>
       </View>
       )}
-      <Text style={styles.versionText}>Версія 1.3.2 no report</Text>
+      <Text style={styles.versionText}>no report</Text>
     </View>
   );
 };
